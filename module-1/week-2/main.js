fc3b9354c11babd4b9b70aeaab423f5d69b4650a
xDouglas90 <xdouglas90@gmail.com>
2022-07-06 08:37:02 -0300

    [M1S02] Ex 4 - Listagem de eventos

--- module-1/week-2/main.js
+++ module-1/week-2/main.js
@@ -1,3 +1,4 @@
+// Menu mobile
 const toggleBtn = document.querySelector('.topbar__navbar-button'),
   navbar = document.querySelector('.topbar__navbar-list');
 
@@ -6,3 +7,41 @@ const handleToggle = () => {
     navbar.classList.toggle('topbar__navbar-list--open');
   });
 };
+
+// Slider
+window.addEventListener('load', function () {
+  new Glider(document.querySelector('.glider'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '#resp-dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next',
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 'auto',
+          itemWidth: 150,
+          duration: 0.25,
+        },
+      },
+      {
+        // screens greater than >= 992px
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 'auto',
+          itemWidth: 350,
+          duration: 0.25,
+        },
+      },
+    ],
+  });
+});
