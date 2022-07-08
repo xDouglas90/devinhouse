b3335c1c4a4dc34c3ccffcfa7e9c3cc991d3437b
xDouglas90 <xdouglas90@gmail.com>
2022-07-07 22:05:58 -0300

    chore: improves code reading with comments

--- module-1/week-2/years-difference/main.js
+++ module-1/week-2/years-difference/main.js
@@ -1,18 +1,21 @@
 window.onload = function () {
+  // Get user year by a prompt
   const year = prompt('Digite um ano');
+  // Get the current year
   const year2 = new Date().getFullYear();
-
+  // Calculate the difference between the years
   const yearsDifference = year2 - year;
-
+  // Show the difference in the alert
   alert(`${year2} - ${year} = ${yearsDifference}`);
-
+  // Add user year in HTML table
   const yearTd = document.querySelector('.user-year');
   yearTd.textContent = `${year}`;
-
+  // Add years difference in HTML table
   const resultTd = document.querySelector('.result');
   resultTd.textContent = `${yearsDifference} anos de diferença`;
 };
 
+// Function to refresh page 
 const refresh = () => {
   location.reload();
 };
