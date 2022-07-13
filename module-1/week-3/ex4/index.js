add94d1da361a2d728b0c63705e6bd48429124e5
xDouglas90 <xdouglas90@gmail.com>
2022-07-12 22:06:35 -0300

    chore: cria objetos dos convidados

---
+++ module-1/week-3/ex4/index.js
@@ -0,0 +1,41 @@
+const guestOne = {
+  nome: 'Douglas',
+  sobrenome: 'Oliveira',
+  setor: 'Camarote',
+  idade: 32,
+};
+
+const guestTwo = {
+  nome: 'Ana',
+  sobrenome: 'Freire',
+  setor: 'Camarote',
+  idade: 25,
+};
+
+const guestThree = {
+  nome: 'Júlia',
+  sobrenome: 'Santos',
+  setor: 'Arquibancada',
+  idade: 10,
+};
+
+const guestFour = {
+  nome: 'Monique',
+  sobrenome: 'Oliveira',
+  setor: 'Pista',
+  idade: 25,
+};
+
+const guestFive = {
+  nome: 'Monise',
+  sobrenome: 'Oliveira',
+  setor: 'Pista',
+  idade: 22,
+};
+
+const guestSix = {
+  nome: 'Miquéias',
+  sobrenome: 'Souza',
+  setor: 'Arquibancada',
+  idade: 11,
+};
